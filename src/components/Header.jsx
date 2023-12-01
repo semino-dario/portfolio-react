a7680218087ca85f227b5701030c3a8c2ac4ed39
Dario Semino <orbislibroscontacto@gmail.com>
2023-12-01 12:08:50 -0300

    image header md in px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="md:mb-[200px]">
             <div className="flex mt-5  ">
                 <div className="">
-                    <img src={RetratoDario} className="rounded-full w-40 h-40 object-cover border-2 border-titulo md:w-60 md:h-60 " alt="Darío Semino's photo" />
+                    <img src={RetratoDario} className="rounded-full w-40 h-40 object-cover border-2 border-titulo md:w-[230px] md:h-[230px] " alt="Darío Semino's photo" />
                 </div>
                 <div className="w-62 flex flex-col items-center font-bold mt-14 md:ml-[-100px]">
                     <h1 className="text-2xl uppercase  text-2xl text-titulo mt-[-50px] md:text-6xl">Portfolio</h1>
