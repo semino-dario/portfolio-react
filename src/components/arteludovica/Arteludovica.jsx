77ff15d82102dd45ca5470a15c8c7c84a63b72c3
Dario Semino <orbislibroscontacto@gmail.com>
2024-04-03 16:01:39 -0300

    main arteludovica sin ssl

--- src/components/arteludovica/Arteludovica.jsx
+++ src/components/arteludovica/Arteludovica.jsx
@@ -29,7 +29,7 @@ export default function Arteludovica() {
                         imagenes={null}
                         generalInfo={'On this site, all the material from the Arteludovica Theater Company is available, of which I was a part several years ago. There are galleries of images, videos, texts, and PDF downloads. In addition to the main site, there are three sections or subsites corresponding to each of the theater productions premiered by the company. In each case, the design of the section varies to adapt to the aesthetics of the respective play.'}
                         techInfo={'This is the second site I developed with HTML, CSS, and Javascript on the frontend, along with a simple backend developed with PHP and a SQL database. The project involved a focus on design, leading me to go deeper into CSS, Figma, and the creation of custom icons using SVG.'}
-                        site={'https://arteludovica.com.ar/'}
+                        site={'http://arteludovica.com.ar/'}
                         repo={'https://github.com/semino-dario/arteludovica'}
                     />
                     <br />
