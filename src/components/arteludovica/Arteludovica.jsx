0e3094272326ef3f3582a268cbbd651c42958c5a
Dario Semino <orbislibroscontacto@gmail.com>
2024-04-15 22:39:32 -0300

    arteludovica text corrected

--- src/components/arteludovica/Arteludovica.jsx
+++ src/components/arteludovica/Arteludovica.jsx
@@ -10,7 +10,6 @@ export default function Arteludovica() {
     return (
 
         <div className='shadow-2xl bg-negroGris pb-[5rem]'>
-
             <div className=" flex flex-col justify-around py-12 md:px-4 ">
                 <div className='flex flex-col md:flex-row md:gap-[8rem]'>
                     <div>
@@ -27,7 +26,7 @@ export default function Arteludovica() {
                     <ArteludovicaSites />
                     <Project
                         imagenes={null}
-                        generalInfo={'On this site, all the material from the Arteludovica Theater Company is available, of which I was a part several years ago. There are galleries of images, videos, texts, and PDF downloads. In addition to the main site, there are three sections or subsites corresponding to each of the theater productions premiered by the company. In each case, the design of the section varies to adapt to the aesthetics of the respective play.'}
+                        generalInfo={'On this site, all the material from the Arteludovica Theater Company is available. There are galleries of images, videos and PDF downloads. There are also three sections or subsites corresponding to each of the theater productions premiered by the company. In each case, the design of the section varies to adapt to the aesthetics of the respective play.'}
                         techInfo={'This is the second site I developed with HTML, CSS, and Javascript on the frontend, along with a simple backend developed with PHP and a SQL database. The project involved a focus on design, leading me to go deeper into CSS, Figma, and the creation of custom icons using SVG.'}
                         site={'http://arteludovica.com.ar/'}
                         repo={'https://github.com/semino-dario/arteludovica'}
