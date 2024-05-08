725c912d20ce97af47a1759432aeb9fef8332aaf
Dario Semino <orbislibroscontacto@gmail.com>
2024-05-08 18:24:27 -0300

    new link orbis

--- src/components/orbis-libris/OrbisLibrisImage.jsx
+++ src/components/orbis-libris/OrbisLibrisImage.jsx
@@ -20,7 +20,7 @@ export default function OrbisLibrisImage() {
 
 
     return (
-        <div className=" bg-[#000000] flex flex-col justify-center align-center py-[4rem]">
+        <div className=" bg-[#000000] flex flex-col justify-center align-center gap-6 py-[4rem]">
             <div ref={containerRef} className=" relative w-[90vw] md:w-[50vw]">
                 <div className="absolute ">
                     <img className={'w-[100%]'} src={orbis2} alt="" />
@@ -31,12 +31,14 @@ export default function OrbisLibrisImage() {
             </div>
             <br />
             <br />
-            <div className='mt-[70%] md:mt-[43%] flex flex-col justify-center align-center text-[#FFFFFF]'>
+            <div className='mt-[70%] md:mt-[43%] flex flex-col justify-center align-center text-[#FFFFFF] gap-6'>
                 <p className=" text-azulClaro text-center font-extrabold text-4xl ">Do you like books?</p>
                 <br />
-                <p className='text-center font-serif text-xl transition-opacity transform'>My personal website for book reviews. <br /> Built with Wordpress and Elementor.</p>
+                <div className='text-center font-serif text-xl transition-opacity transform px-4 flex flex-col justify-center items-center gap-2'>
+                    <p >My personal website for book reviews.</p> <p> It was originally developed with Wordpress, and then I re-built it using Next.js with Typescript. <br /></p>
+                </div>
                 <br />
-                <a href="https://orbislibris.com/" target="_blank" className="text-xl text-titulo hover:text-azulClaro"> - visit site -</a>
+                <a href="https://orbislibris.netlify.app/" target="_blank" className="text-xl text-titulo hover:text-azulClaro"> - visit site -</a>
             </div>
         </div>
 
